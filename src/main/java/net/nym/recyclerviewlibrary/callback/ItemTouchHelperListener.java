2d70054cb640cf6ae0d5c239d447b29476cf4917
niyueming <niyueming@163.com>
2017-03-14 17:47:07 +0800

    增加上下拖动item，和滑动删除item

---
+++ src/main/java/net/nym/recyclerviewlibrary/callback/ItemTouchHelperListener.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017  Ni YueMing<niyueming@163.com>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+package net.nym.recyclerviewlibrary.callback;
+
+/**
+ * @author niyueming
+ * @date 2017-03-14
+ * @time 17:37
+ */
+
+public interface ItemTouchHelperListener {
+
+    void onItemMove(int fromPosition, int toPosition);
+
+    void onItemSwiped(int position);
+}
