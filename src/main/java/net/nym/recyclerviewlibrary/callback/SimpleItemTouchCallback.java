7d0fb302550a4d5f474fdfd0c3696f7b10362a53
niyueming <niyueming@163.com>
2017-03-15 10:28:46 +0800

    优化

--- src/main/java/net/nym/recyclerviewlibrary/callback/SimpleItemTouchCallback.java
+++ src/main/java/net/nym/recyclerviewlibrary/callback/SimpleItemTouchCallback.java
@@ -11,6 +11,7 @@
 
 package net.nym.recyclerviewlibrary.callback;
 
+import android.graphics.Canvas;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -32,7 +33,7 @@ public class SimpleItemTouchCallback extends ItemTouchHelper.Callback {
 
     @Override
     public boolean isLongPressDragEnabled() {
-        return false;   //false：不需要长按拖拽功能，手动控制；true：长按触发拖拽功能
+        return true;   //false：不需要长按拖拽功能，手动控制；true：长按触发拖拽功能
     }
 
     @Override
@@ -50,7 +51,7 @@ public class SimpleItemTouchCallback extends ItemTouchHelper.Callback {
         } else {
             dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
         }
-        // 如果想支持滑动(删除)操作, swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END //左右滑动
+        // 如果想支持滑动(删除)操作, swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END；swipeFlags = 0不支持滑动 //左右滑动
         int swipeFlags = 0;
         return makeMovementFlags(dragFlags, swipeFlags);
     }
@@ -74,5 +75,57 @@ public class SimpleItemTouchCallback extends ItemTouchHelper.Callback {
         if (mListener != null){
             mListener.onItemSwiped(viewHolder.getAdapterPosition());
         }
+    }
+
+    /**
+     * 在每次View Holder的状态变成拖拽 (ACTION_STATE_DRAG) 或者 滑动 (ACTION_STATE_SWIPE)的时候被调用。
+     * 这是把你的item view变成激活状态的最佳地点。
+     * @param viewHolder
+     * @param actionState
+     */
+    @Override
+    public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+        if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+            viewHolder.itemView.setSelected(true);
+        }
+        super.onSelectedChanged(viewHolder, actionState);
+    }
+
+    /**
+     * 在一个view被拖拽然后被放开的时候被调用，同时也会在滑动被取消或者完成ACTION_STATE_IDLE)的时候被调用。
+     * 这里是恢复item view idle状态的典型地方。
+     * @param recyclerView
+     * @param viewHolder
+     */
+    @Override
+    public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        viewHolder.itemView.setSelected(false);
+        super.clearView(recyclerView, viewHolder);
+    }
+
+    /**
+     * 可以在这自定义动画
+     * @param c
+     * @param recyclerView
+     * @param viewHolder
+     * @param dX
+     * @param dY
+     * @param actionState
+     * @param isCurrentlyActive
+     */
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+//        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {//重写默认的滑动动画
+//            float width = (float) viewHolder.itemView.getWidth();
+//            float alpha = 1.0f - Math.abs(dX) / width;
+//            viewHolder.itemView.setAlpha(alpha);
+//            viewHolder.itemView.setTranslationX(dX);
+//        } else {
+//            super.onChildDraw(c, recyclerView, viewHolder, dX, dY,
+//                    actionState, isCurrentlyActive);
+//        }
+
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY,
+                actionState, isCurrentlyActive);
     }
 }
